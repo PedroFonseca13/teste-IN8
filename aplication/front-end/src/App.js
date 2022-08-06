02d31f078ebb3365104dc7d8178f9d595a515dfc
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-08-06 10:40:24 -0300

    refactor: alterado os estilos do App.css && renderização de registration em App.js

--- aplication/front-end/src/App.js
+++ aplication/front-end/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './pages/Home';
+import Registrator from './pages/registration';
 
 function App() {
 	return (
 		<div className="App">
 			<Home />
+			<Registrator />
 		</div>
 	);
 }
