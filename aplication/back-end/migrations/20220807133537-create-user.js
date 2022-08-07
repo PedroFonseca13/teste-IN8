1361e5d5e604946d1b3c6e57d88a9a21bceef3e6
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-08-07 14:12:35 -0300

    feat: add back-end

---
+++ aplication/back-end/migrations/20220807133537-create-user.js
@@ -0,0 +1,49 @@
+'use strict';
+
+//JSdocs
+/**
+ * @param {import('sequelize').queryInterface} queryInterface
+ * @param {import('sequelize').Sequelize} Sequelize
+*/
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      bday: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at'
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at'
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
