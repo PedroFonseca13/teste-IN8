58441b7154b8cf819c6c7082ca26f51aedc8b5c3
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-08-07 20:37:33 -0300

    refactor: implementado a requisição da api

--- aplication/front-end/src/pages/Users/index.js
+++ aplication/front-end/src/pages/Users/index.js
@@ -1,37 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getUser } from '../../services/api';
 import Footer from '../../components/Footer';
 import Icon from '../../assets/img/topo-pag.svg';
 import './users.css';
 
 function index() {
+	const [users, setUsers] = useState([]);
+
+	const fetchGetUser = async () => {
+		const result = await getUser();
+		setUsers(result);
+	};
+
+	useEffect(() => {
+		fetchGetUser();
+	});
+
+
 	return (
 		<>
 			<div id="table">
 				<div className="container">
 					<h3 className="table-title">Lista de cadastro</h3>
 					<table className="table">
-						<tr>
-							<th></th>
-							<th>nome</th>
-							<th>e-mail</th>
-							<th>nascimento</th>
-							<th>telefone</th>
-						</tr>
-						<tr>
-							<td></td>
-							<td>Fulano</td>
-							<td>fulanobos@gmail.com</td>
-							<td>01/03/1992</td>
-							<td>(31) 9 9974-0420</td>
-						</tr>
-						<tr>
-							<td className="td-bottom"></td>
-							<td className="td-bottom"></td>
-							<td className="td-bottom"></td>
-							<td className="td-bottom"></td>
-							<td className="td-bottom"></td>
-						</tr>
+						<thead>
+							<tr>
+								<th></th>
+								<th>nome</th>
+								<th>e-mail</th>
+								<th>nascimento</th>
+								<th>telefone</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							{ users.map(user => (
+								<tr key={ user.id }>
+									<td></td>
+									<td>{ user.name }</td>
+									<td>{ user.email }</td>
+									<td>{ user.bday }</td>
+									<td>{ user.phone }</td>
+									<td></td>
+								</tr>
+							)) }
+
+							<tr>
+								<td className="td-bottom"></td>
+								<td className="td-bottom"></td>
+								<td className="td-bottom"></td>
+								<td className="td-bottom"></td>
+								<td className="td-bottom"></td>
+								<td className="td-bottom"></td>
+							</tr>
+						</tbody>
 					</table>
 					<span>
 						<Link to="/">
